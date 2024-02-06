0ad20f958c6ceacc72473d01ca83f1f63d1aa07d
OR-11 <93813653+OR-11@users.noreply.github.com>
2024-02-06 22:20:59 +0900

    プロジェクト ファイルを追加します。

---
+++ EngineControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ustPasser
+{
+    public class EngineControl
+    {
+        public static int BootEngine()//1:failed,0:scceeded
+        {
+            //string user = Environment.UserName;
+            System.Diagnostics.Process result=null;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))//windows
+            {
+                try
+                {
+                    result = System.Diagnostics.Process.Start(System.Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Programs\VOICEVOX\vv-engine\run.exe");
+                }
+                catch (Exception)
+                {
+                    result = null;
+                }
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))//MacOS
+            {
+                try
+                {
+                    result = System.Diagnostics.Process.Start(@"/Applications/VOICEVOX/vv-engine/run.app");
+                }
+                catch (Exception)
+                {
+                    try
+                    {
+                        result = System.Diagnostics.Process.Start(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"/Applications/VOICEVOX/vv-engine/run.app");
+                    }
+                    catch (Exception)
+                    {
+                        result = null;
+                    }
+                }
+            }
+            else return 1;//誰やねんお前
+            if (result == null) return 1;
+            else return 0;
+        }
+        
+    }
+}
