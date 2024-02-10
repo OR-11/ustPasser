f136e8f81b406a3805bb22e61841bb7621d8699a
OR-11 <93813653+OR-11@users.noreply.github.com>
2024-02-10 23:11:45 +0900

    キー変更機能を追加

--- Generate.cs
+++ Generate.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Text.Json;
 
 namespace ustPasser
 {
@@ -17,28 +18,68 @@ namespace ustPasser
 
         //frame_length仕様
         //https://github.com/VOICEVOX/voicevox/blob/main/src/sing/domain.ts#L105
-        public static string SingFrameAudioQuery(int speaker, string core_version, Note[] notes)
+        public static string SingFrameAudioQuery(int speaker, string core_version, Note[] notes, float tempo)
         {//https://qiita.com/rawr/items/f78a3830d894042f891b
             using var Http = new HttpClient();
             string jsonContent =@"{""notes"":[";
 
-            int restDuration = 1;//とりあえず1らしい
+            //https://github.com/VOICEVOX/voicevox/blob/55d3df8712b54360d2428a0954fcd9d33f67dd16/src/store/singing.ts#L877
+            int restDurationSeconds = 1;//とりあえず1らしい
+            decimal frameRate = JsonToLibary.ParseJson(VoiceVoxEngineControl.EngineManifest())["frame_rate"];
+            int restFrameLength = Convert.ToInt32(Math.Round(restDurationSeconds * frameRate,MidpointRounding.AwayFromZero));
+            List<Note> notesForRequestToEngine = new List<Note>();
             //最初に休符を追加
+            notesForRequestToEngine.Add(new Note(null, restFrameLength, ""));
 
-            for (int i = 0; i < notes.Length; i++)
+            //int frame = 0;
+            foreach (Note note in notes)
             {
-                var j = notes[i];
-                if (i != 0) jsonContent += ",";
-                jsonContent += @"{""key"":" + j.key + @",""frame_length"":" + j.frame_length + @",""lyric"":""" + j.lyric + @"""}";
+                //Console.WriteLine(Convert.ToInt32(Math.Round(60 / (decimal)tempo * (decimal)note.length * frameRate, MidpointRounding.AwayFromZero)));
+                notesForRequestToEngine.Add(new Note(note.key,Convert.ToInt32(Math.Round(60/(decimal)tempo*(decimal)note.length*frameRate,MidpointRounding.AwayFromZero)),note.lyric));
             }
+            //最後にも休符を追加
+            notesForRequestToEngine.Add(new Note(null, restFrameLength, ""));
+
+            for (int i = 0; i < notesForRequestToEngine.Count; i++)
+            {
+                var j = notesForRequestToEngine[i];
+                if (i != 0) jsonContent += ",";
+                jsonContent += @"{""key"":" + (j.key==null?"null":j.key.Value) + @",""frame_length"":" + j.length + @",""lyric"":""" + j.lyric + @"""}";
+            }
+
             jsonContent += @"]}";
             Console.WriteLine(jsonContent);
             var content = new StringContent(jsonContent, Encoding.UTF8, @"application/json");
-            return Http.PostAsync(@"http://127.0.0.1:50021/sing_frame_audio_query?speaker=" + speaker + @"&core_version=" + core_version, content).Result.Content.ReadAsStringAsync().Result;
+            var result = Http.PostAsync(@"http://127.0.0.1:50021/sing_frame_audio_query?speaker=" + speaker + @"&core_version=" + core_version, content).Result;
+            if (((int)result.StatusCode)==200) return result.Content.ReadAsStringAsync().Result;
+            else
+            {
+                Console.WriteLine(result.Content.ReadAsStringAsync().Result);
+                return "";
+            }
         }
+        
+        public static byte[] FrameSynthesis(int speaker, string core_version, string bodyData, int key = 0)
+        {
+            if (key != 0)
+            {
+                var temp = JsonToLibary.ParseJson(bodyData);
+                Console.WriteLine(temp.GetType());
+                double[] fs = new double[temp["f0"].Count];
+                for (int i = 0;i< temp["f0"].Count; i++)
+                {
+                    fs[i] = Decimal.ToDouble(temp["f0"].ToArray()[i]);
+                }
+                double[] changedFs = new double[fs.Length];
+                for (int i = 0; i < fs.Length; i++)
+                {
+                    changedFs[i] = fs[i] * Math.Pow(2, key / 12);
+                }
+                temp["f0"] = changedFs;
+                bodyData = JsonSerializer.Serialize(temp);
+            }
 
-        public static byte[] FrameSynthesis(int speaker, string core_version, string bodyData)
-        {//https://qiita.com/c-yan/items/6e506399675e3cc56732
+            //https://qiita.com/c-yan/items/6e506399675e3cc56732
             using var Http = new HttpClient();
             //var content = new StringContent(bodyData, Encoding.UTF8, @"application/json");
             var request = new HttpRequestMessage(HttpMethod.Post, @"http://127.0.0.1:50021/frame_synthesis?speaker=" + speaker + @"&core_version=" + core_version);
@@ -51,14 +92,14 @@ namespace ustPasser
 
     public class Note
     {
-        public Note(int key, int frame_length, string lyric)
+        public Note(int? key, int length, string lyric)
         {
             this.key = key;
-            this.frame_length = frame_length;
+            this.length = length;
             this.lyric = lyric;
         }
-        public int key;
-        public int frame_length;
+        public int? key;
+        public int length;
         public string lyric;
     }
 }
